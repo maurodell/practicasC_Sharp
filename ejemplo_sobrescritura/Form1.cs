bc1e13bc633232d88d7960c28b0d6c5e60bb7f29
maurodell <delloliomauro@gmail.com>
2021-09-30 14:57:05 -0300

    Primer commit de sobreescritura/modificadores

---
+++ ejemplo_sobrescritura/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ejemplo_sobrescritura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cuadrado _c = new Cuadrado(int.Parse(Interaction.InputBox("Ingrese un número: ")));
+            MessageBox.Show("El resultado es: " + _c.Area()) ;
+        }
+
+        abstract class Forma
+        {
+            abstract public int Area();
+        }
+        class Cuadrado : Forma
+        {
+            int lado = 0;
+            public Cuadrado(int n) { lado = n; }
+
+            public override int Area()
+            {
+                return lado * lado;
+            }
+        }
+    }
+}
---
+++ ejemplo_sobrescritura/Impuesto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_sobrescritura
+{
+    abstract class Impuesto//creo la clase abstract
+    {
+        public decimal Importe { get; set; }//atributo Importe
+        virtual public decimal ImpuestoEnPesos() { return this.Importe * 0.10m; }//metodo virtual que retorna el importe *0.10
+        abstract public decimal TotalAPagar();//metodo abstracto vacío
+    }
+    class ImpuestoComun : Impuesto
+    {
+        override public decimal TotalAPagar()//hereda el metodo vacio y lo sobrescribe
+        {
+            return Importe + ImpuestoEnPesos();//suma el atributo y el lo que devuelve el metodo.
+        }
+    }
+    class ImpuestoEspecial : Impuesto
+    {
+        override public decimal ImpuestoEnPesos()//se hace override, sobrescribiendo el metodo abstracto
+        {
+            return this.Importe * 0.20m;//se retorna el importe de la clase base
+        }
+        private decimal TasaAdicional()
+        {
+            return this.Importe * 0.01m;
+        }
+        public override decimal TotalAPagar()
+        {
+            return Importe + ImpuestoEnPesos() + TasaAdicional();
+        }
+    }
+}
